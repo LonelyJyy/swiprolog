a44436eeef50c98d7e1de3c5554793a1561a9183
LonelyJyy <156589863+LonelyJyy@users.noreply.github.com>
2025-03-27 10:49:53 +0700

    Add files via upload

---
+++ 1.pl
@@ -0,0 +1,7 @@
+ibukota(jakarta,indonesia).
+ibukota(canberra,australia).
+ibukota(ottawa,canada).
+ibukota(london,uk).
+ibukota(berlin,jerman).
+
+
---
+++ 4.pl
@@ -0,0 +1,14 @@
+anakibu(andi).
+anakibu(budi).
+anakibu(cika).
+anakibu(dani).
+anakibu(emil).
+not(anakibu(fadil)).
+
+sukaPermen(andi).
+sukaPermen(budi).
+sukaPermen(cika).
+not(sukaPermen(dani)).
+not(sukaPermen(emil)).
+
+
